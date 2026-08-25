8b5ac024799a43c10967845a8710392b430ba052
Enzo Vezzaro <enzo-vezzaro@live.it>
2026-08-25 19:45:35 -0400

    fix: resolve package.json version imports for CI (no parent dir)

--- docs/.vitepress/theme/ReposellLayout.vue
+++ docs/.vitepress/theme/ReposellLayout.vue
@@ -3,7 +3,7 @@ import { ref, watch, watchEffect, onMounted, onBeforeUnmount } from 'vue'
 import { useData, withBase } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { initLandingMotion } from './landingMotion'
-import pkg from '../../../../../package.json'
+import pkg from '../../../package.json'
 
 const Layout = DefaultTheme.Layout
 
